8f4ecd87759a8d2e406bfff4d75525652080d85a
Chou VANG <lesolo91@hotmail.com>
2023-11-16 23:31:00 +0100

    maj

--- studi_project/studi_project/settings.py
+++ studi_project/studi_project/settings.py
@@ -35,9 +35,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://chouvang-projet-studi-cb20d2563faa.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
